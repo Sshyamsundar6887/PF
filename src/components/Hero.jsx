665444209a26b48dd37f514dfcc43704ef05c0bd
SHYAM <shyamsriperumbuduru@gmail.com>
2025-02-24 18:19:58 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
                 <div className="flex items-center gap-3"> 
                     <figure className="img-box w-9 h-9 rounded-lg">
                         <img
-                         src="/Law.jpg"
+                         src="./Law.jpg"
                          width={40}
                          height={40}
                          alt="Shyam portrait"
