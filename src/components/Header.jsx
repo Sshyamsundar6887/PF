8232ff94c60d286ccee8b8e2d42b7c3da05c06ea
SHYAM <shyamsriperumbuduru@gmail.com>
2025-02-24 21:09:44 +0530

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,7 +32,7 @@ const Header = () => {
             <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md-grid md:grid-cols-[1fr,3fr,1fr] relative">            
                 <h1>
                     <a 
-                        href="/PF/" 
+                        href="/PF/#" 
                         className="logo"
                     >
                         <img
